eb873cd576f794fdcd1381e78c14ad2fad1a1d8a
fderose <fderose@guidewire.com>
2016-05-12 18:42:05 -0700

    Get rid of unused variable.

--- src/main/Health.java
+++ src/main/Health.java
@@ -6,8 +6,6 @@ import java.util.Map;
 import java.util.Set;
 
 public class Health {
-  private static final Object object = new Object();
-
   private final Map<Exception, Long> exceptionCache = new HashMap<>();
   private boolean healthy = true;
 
